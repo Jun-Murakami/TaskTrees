0f261168dc1f4a862adfde0e7b070aabbbfd65e7
Jun Murakami <126404131+Jun-Murakami@users.noreply.github.com>
2024-03-11 21:51:19 +0900

    TaskTreesロゴのスタイルを修正

--- src/components/TaskTreesLogo.tsx
+++ src/components/TaskTreesLogo.tsx
@@ -3,7 +3,7 @@ import TaskTreesSVG from '/TaskTrees.svg';
 
 export const TaskTreesLogo = () => {
   return (
-    <>
+    <Box sx={{ textAlign: 'center' }}>
       <Typography sx={{ marginBottom: 0 }} variant='h3'>
         <img src={TaskTreesSVG} alt='Task Tree' style={{ width: '35px', height: '35px', marginRight: '10px' }} />
         TaskTrees
@@ -13,6 +13,6 @@ export const TaskTreesLogo = () => {
           Team Edition
         </Typography>
       </Box>
-    </>
+    </Box>
   );
 };
