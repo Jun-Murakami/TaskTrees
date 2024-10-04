6a685a5d1855ef0fbdf225b258404cf00d88b7de
Jun-Murakami <contact@bucketrelay.com>
2024-10-04 21:51:30 +0900

    feat: Fix authentication provider redirect loop
    Fix the issue where the authentication provider was causing a redirect loop by checking the `authProvider` value from local storage before initiating the authentication flow. If the `authProvider` is already set to either 'apple' or 'google', the `AuthElectronApple` and `AuthElectronGoogle` components will remove the `authProvider` value from local storage and return early. This prevents the redirect loop and ensures a smooth authentication process.

--- src/features/app/AuthElectronApple.tsx
+++ src/features/app/AuthElectronApple.tsx
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
 import { getAuth, signInWithRedirect, OAuthProvider } from 'firebase/auth';
 
+const authProvider = localStorage.getItem('auth_provider');
+
 export const AuthElectronApple = () => {
   useEffect(() => {
+    if (authProvider === 'apple') {
+      localStorage.removeItem('auth_provider');
+      return;
+    }
     const provider = new OAuthProvider('apple.com');
     const auth = getAuth();
+    localStorage.setItem('auth_provider', 'apple');
     signInWithRedirect(auth, provider);
   }, []);
 
--- src/features/app/AuthElectronGoogle.tsx
+++ src/features/app/AuthElectronGoogle.tsx
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
 import { getAuth, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth';
 
+const authProvider = localStorage.getItem('auth_provider');
+
 export const AuthElectronGoogle = () => {
   useEffect(() => {
+    if (authProvider === 'google') {
+      localStorage.removeItem('auth_provider');
+      return;
+    }
     const provider = new GoogleAuthProvider();
     const auth = getAuth();
+    localStorage.setItem('auth_provider', 'google');
     signInWithRedirect(auth, provider);
   }, []);
 
