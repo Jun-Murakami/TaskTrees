5327205014b80fd46886c0ab47d9c83c2d0ce604
Jun-Murakami <contact@bucketrelay.com>
2024-10-04 22:19:21 +0900

    feat: Update AuthElectronApple and AuthElectronGoogle components
    Refactor the AuthElectronApple and AuthElectronGoogle components to handle authentication with Apple and Google providers respectively. This includes removing the authProvider value from local storage and returning early if the authProvider is already set to 'apple' or 'google', preventing a redirect loop. These changes improve the authentication flow and ensure a smooth user experience.

--- src/features/app/AuthElectronApple.tsx
+++ src/features/app/AuthElectronApple.tsx
@@ -5,17 +5,19 @@ const authProvider = localStorage.getItem('auth_provider');
 
 export const AuthElectronApple = () => {
   useEffect(() => {
-    if (authProvider === 'apple') {
-      localStorage.removeItem('auth_provider');
-      return;
-    }
     getRedirectResult(getAuth()).then((result) => {
       if (result) {
         const credential = OAuthProvider.credentialFromResult(result);
         const url = '/auth/redirect?credential=' + JSON.stringify(credential);
+        localStorage.removeItem('auth_provider');
         window.location.href = url;
       }
     });
+    if (authProvider === 'apple') {
+      localStorage.removeItem('auth_provider');
+      return;
+    }
+
     const provider = new OAuthProvider('apple.com');
     const auth = getAuth();
     localStorage.setItem('auth_provider', 'apple');
--- src/features/app/AuthElectronGoogle.tsx
+++ src/features/app/AuthElectronGoogle.tsx
@@ -5,17 +5,18 @@ const authProvider = localStorage.getItem('auth_provider');
 
 export const AuthElectronGoogle = () => {
   useEffect(() => {
-    if (authProvider === 'google') {
-      localStorage.removeItem('auth_provider');
-      return;
-    }
     getRedirectResult(getAuth()).then((result) => {
       if (result) {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const url = '/auth/redirect?credential=' + JSON.stringify(credential);
+        localStorage.removeItem('auth_provider');
         window.location.href = url;
       }
     });
+    if (authProvider === 'google') {
+      localStorage.removeItem('auth_provider');
+      return;
+    }
 
     const provider = new GoogleAuthProvider();
     const auth = getAuth();
